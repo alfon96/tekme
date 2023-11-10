d4a2285257b8e05e1ae2328507d5b2710191b5bb
alfo96 <alfalcone1996@gmail.com>
2023-11-10 01:17:45 +0100

    add fetch data from  different months

--- frontend/src/components/Page/FetchPage.js
+++ frontend/src/components/Page/FetchPage.js
@@ -32,28 +32,34 @@ const FetchPage = (props) => {
             setIsLoading(false);
         }
         simpleFetch();
-    }, [fetchingUri]);
+    }, [fetchingUri, fetchingMonth]);
 
     const handleMonthSelection = (next) => {
+        console.log("Il valore dellinput è : ");
+        console.log(next);
         const actualSelectedMonth = fetchingMonth;
         if (next) {
-            if (actualSelectedMonth != 12) {
+            if (actualSelectedMonth !== 12) {
                 setFetchingMonth((prevValue) => prevValue + 1);
-                setFetchingURI(`http://localhost:8000/teachers/Spopovic/${actualSelectedMonth + 1}}`);
+                setFetchingURI(`http://localhost:8000/teachers/Spopovic/${actualSelectedMonth + 1}`);
             }
             else {
+                console.log("Il mese è 12 e vuoi andare avanti")
+
                 setFetchingMonth(1);
-                setFetchingURI(`http://localhost:8000/teachers/Spopovic/${1}}`);
+                setFetchingURI(`http://localhost:8000/teachers/Spopovic/1`);
             }
         }
-
-        if (actualSelectedMonth != 1) {
-            setFetchingMonth((prevValue) => prevValue - 1);
-            setFetchingURI(`http://localhost:8000/teachers/Spopovic/${actualSelectedMonth - 1}}`);
-        }
         else {
-            setFetchingMonth(1);
-            setFetchingURI(`http://localhost:8000/teachers/Spopovic/${1}}`);
+            if (actualSelectedMonth !== 1) {
+                setFetchingMonth((prevValue) => prevValue - 1);
+                setFetchingURI(`http://localhost:8000/teachers/Spopovic/${actualSelectedMonth - 1}`);
+            }
+            else {
+                console.log("Il mese è 1 e vuoi andare indietro")
+                setFetchingMonth(12);
+                setFetchingURI(`http://localhost:8000/teachers/Spopovic/12`);
+            }
         }
 
     }
